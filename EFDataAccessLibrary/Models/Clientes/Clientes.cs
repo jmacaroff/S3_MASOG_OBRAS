04d8733c5c40d3bba2948d8c777d300ec7bdd405
lucas gomez <lugomezzu@gmail.com>
2020-09-16 23:40:59 -0300

    creacion de la clase modelo de cliente

---
+++ EFDataAccessLibrary/Models/Clientes/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace EFDataAccessLibrary.Models.Clientes
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public double MyP { get; set; }
+    }
+}
