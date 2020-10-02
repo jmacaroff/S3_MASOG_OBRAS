0a3949ed46d8c7bd74dc8e7f123bdc0d867ab400
Joaquin Antonio Macaroff Perez <jomax1398@gmail.com>
2020-10-02 14:04:38 -0300

    Primer CRUD de FacturaCompra

---
+++ MASOG_OBRAS/Pages/Compras/FacturaDeCompra/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models.Compras;
+
+namespace MASOG_OBRAS.Pages.Compras.FacturaDeCompra
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly EFDataAccessLibrary.DataAccess.ProductContext _context;
+
+        public DetailsModel(EFDataAccessLibrary.DataAccess.ProductContext context)
+        {
+            _context = context;
+        }
+
+        public FacturaCompra FacturaCompra { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FacturaCompra = await _context.FacturasCompra
+                .Include(f => f.Proveedor).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (FacturaCompra == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ MASOG_OBRAS/Pages/Compras/FacturaDeCompra/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models.Compras;
+
+namespace MASOG_OBRAS.Pages.Compras.FacturaDeCompra
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EFDataAccessLibrary.DataAccess.ProductContext _context;
+
+        public IndexModel(EFDataAccessLibrary.DataAccess.ProductContext context)
+        {
+            _context = context;
+        }
+
+        public IList<FacturaCompra> FacturaCompra { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            FacturaCompra = await _context.FacturasCompra
+                .Include(f => f.Proveedor).ToListAsync();
+        }
+    }
+}
