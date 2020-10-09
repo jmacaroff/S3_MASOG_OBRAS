6bc7fc2ab9f1f006d0aee31e541c8952ad1526c0
Joaquin Antonio Macaroff Perez <jomax1398@gmail.com>
2020-10-09 16:48:40 -0300

    Mejora UI de Ordenes de pago y Sacado los editar en Detalles

--- MASOG_OBRAS/Pages/Compras/OrdenesPago/Index.cshtml.cs
+++ MASOG_OBRAS/Pages/Compras/OrdenesPago/Index.cshtml.cs
@@ -7,10 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EFDataAccessLibrary.DataAccess;
 using EFDataAccessLibrary.Models.Compras;
+using MASOG_OBRAS.Classes;
 
 namespace MASOG_OBRAS.Pages.Compras.OrdenesPago
 {
-    public class IndexModel : PageModel
+    public class IndexModel : BaseIndexPage<OrdenPago>
     {
         private readonly EFDataAccessLibrary.DataAccess.ProductContext _context;
 
@@ -19,13 +20,64 @@ namespace MASOG_OBRAS.Pages.Compras.OrdenesPago
             _context = context;
         }
 
-        public IList<OrdenPago> OrdenPago { get;set; }
+        public PaginatedList<OrdenPago> OrdenesPago { get;set; }
 
-        public async Task OnGetAsync()
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            OrdenPago = await _context.OrdenesPago
-                .Include(o => o.ConceptoPago)
-                .Include(o => o.Proveedor).ToListAsync();
+            //inicializo el orden actual
+            CurrentSort = sortOrder;
+
+            // controlamos los filtros de busqueda
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            //Agrego filtro de busqueda
+            CurrentFilter = searchString;
+
+            IQueryable<OrdenPago> ordenesIQ = from o in _context.OrdenesPago select o;
+
+            //veo si el buscador esta vacio para poder realizar la búsqueda
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ordenesIQ = ordenesIQ.Where(c => c.FechaEmision.ToString().Contains(searchString));
+            }
+
+            ////analizo los casos para el ordenamiento
+            switch (sortOrder)
+            {
+                //    case "precio_desc":
+                //        productosIQ = productosIQ.OrderBy(p => p.Precio);
+                //        break;
+                //    case "name_dec":
+                //        productosIQ = productosIQ.OrderByDescending(p => p.Descripcion);
+                //        break;
+                default:
+                    ordenesIQ = ordenesIQ.OrderBy(c => c.Id);
+                    break;
+            }
+
+            int pageSize = 5;
+
+            // Se agrega Include(c => c.Proveedor) para que recupere los datos del proveedor asociado a la orden
+
+            OrdenesPago = await PaginatedList<OrdenPago>.CreateAsync(ordenesIQ.Include(o => o.ConceptoPago)
+                                                                              .Include(o => o.Proveedor)
+                                                                              .AsNoTracking(), pageIndex ?? 1, pageSize);
+
+            //Orden de Pago
+            //OrdenPago = await _context.OrdenesPago
+            //  .Include(o => o.ConceptoPago)
+            //  .Include(o => o.Proveedor).ToListAsync();
         }
     }
 }
